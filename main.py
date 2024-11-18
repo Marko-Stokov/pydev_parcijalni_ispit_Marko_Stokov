a1bf044b59b483bbc5435ac8255de3ddbcc152c0
Marko Stokov <stokovmarko@gmail.com>
2024-11-18 20:56:48 +0100

    Promjene za parcijalni ispit

--- main.py
+++ main.py
@@ -54,9 +54,43 @@ def manage_customers(customers):
     """
     Allows the user to add a new customer or view all customers.
     """
+    customers = load_data(CUSTOMERS_FILE)
+    while True:
+        print("\nUpravljanje kupcima izbornik:")
+        print("1. Dodavanje novog kupca")
+        print("2. Prikaz liste kupaca")
+        print("3. Izlaz")
+
+        choice = input("Odabrana opcija: ")
+
+        if choice == "1":
+            name = input("Unesite ime kupca: ").strip()
+            email = input("Unesite email kupca: ").strip()
+            vat_id = input("Unesite VAT ID kupca: ").strip()
+
+            novi_kupac = {
+                "name": name,
+                "email": email,
+                "vat_id": vat_id
+            }
+            customers.append(novi_kupac)
+            save_data(CUSTOMERS_FILE, customers)
+
+            print("Kupac uspješno dodan")
+        elif choice == "2":
+            with open(CUSTOMERS_FILE, "r") as file:
+                json_data = json.load(file)
+                print(json_data)
+        elif choice == "3":
+            # Pohrana podataka prilikom izlaza
+            save_data(CUSTOMERS_FILE, customers)
+            break
+        else:
+            print("Krivi izbor. Pokusajte ponovno.")
+
     # Za dodavanje: omogući unos imena kupca, emaila i unos VAT ID-a
     # Za pregled: prikaži listu svih kupaca
-    pass
+
 
 
 # TODO: Implementirajte funkciju za prikaz ponuda.
@@ -64,6 +98,36 @@ def display_offers(offers):
     """
     Display all offers, offers for a selected month, or a single offer by ID.
     """
+    offers = load_data(OFFERS_FILE)
+
+    while True:
+        print("\nIzbornik ponuda:")
+        print("1. Pregledaj sve opcije")
+        print("2. Pogledaj ponude za naznačeni mjesec")
+        print("3. Pogledaj pojedinu ponudu prema ID")
+        print("4. Izlaz")
+        
+        choice = input("Odaberite opciju: ")
+        if choice == "1":
+            with open(OFFERS_FILE, "r") as file:
+                json_data = json.load(file)
+                print(json_data)
+        elif choice == "2":
+            with open(OFFERS_FILE, "r") as file:
+                json_data = json.load(file)
+                print(json_data)
+        elif choice == "3":
+            # Pohrana podataka prilikom izlaza
+            offer_id = input("Unesite ID ponude: ").strip()
+            offer = next((offer for offer in offers if offer['id'] == offer_id), None)
+            if offer:
+                printanje_ponude = print_offer(offer)
+            else:
+                print("Nijedna ponuda pronađena.")
+            break
+        else:
+            print("Krivi izbor. Pokusajte ponovno.")
+
     # Omogućite izbor pregleda: sve ponude, po mjesecu ili pojedinačna ponuda
     # Prikaz relevantnih ponuda na temelju izbora
     pass
